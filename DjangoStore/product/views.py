4951fa6598547b3319532226ddf0e0e47e7c081a
um-shuhaib <shuhaib5560@gmail.com>
2025-06-10 22:21:39 +0530

    product module completed. and pagination learned

--- DjangoStore/product/views.py
+++ DjangoStore/product/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-
+from . models import Product
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def product_list(request):
-    return render(request,'product.html')
+    page=1
+    if request.GET:
+        page = request.GET.get('page',1)
+    product_list=Product.objects.all()
+    product_paginator=Paginator(product_list,2)
+    product_list=product_paginator.get_page(page)
+    context={'product':product_list}
+    return render(request,'product.html',context)
 
 
 def product_details(request):
